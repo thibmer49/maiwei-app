d45a7867f0cab0be19dfb8a192b195067f1a08a9
Til Lombardo <til.lombardo@hotmail.fr>
2023-03-15 10:00:20 +0100

    slider dynamique laid

--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -1,26 +1,29 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["range", "value"];
+
   connect() {
-
-    var rangeSlider = function(){
-      var slider = $('.range-slider'),
-          range = $('.range-slider__range'),
-          value = $('.range-slider__value');
-
-      slider.each(function(){
-
-        value.each(function(){
-          var value = $(this).prev().attr('value');
-          $(this).html(value);
-        });
-
-        range.on('input', function(){
-          $(this).next(value).html(this.value);
-        });
-      });
-    };
-
-    rangeSlider();
-    };
+    this.updateValue();
+    this.positionValueElement();
+    console.log("hello-from-slider")
   }
+
+  updateValue() {
+    const currentValue = parseInt(this.rangeTarget.value);
+    this.valueTarget.innerText = `${currentValue}€`;
+    this.positionValueElement();
+  }
+
+  positionValueElement() {
+    const range = this.rangeTarget;
+    const rangeWidth = range.clientWidth;
+    const min = parseInt(range.min);
+    const max = parseInt(range.max);
+    const currentValue = parseInt(range.value);
+    const position = ((currentValue - min) / (max - min)) * rangeWidth;
+
+    this.valueTarget.style.left = `calc(${position}px - 3%)`;
+
+  }
+}
