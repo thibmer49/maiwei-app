cba600638d74131e847ae537c3cfeab27d0e1d97
Til Lombardo <til.lombardo@hotmail.fr>
2023-03-16 11:44:27 +0100

    front improvements

--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -1,13 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["range", "value", "budget"];
+  static targets = ["range", "value", "budget", "number"];
 
   connect() {
     console.log("hello-from-slider")
   }
 
   updateValue(evt) {
-    this.budgetTarget.value = evt.target.value
+    this.budgetTarget.value = evt.target.value;
+    this.numberTarget.innerText = `${evt.target.value} €`;
   }
 }
