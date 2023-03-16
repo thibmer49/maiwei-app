c07f7f278427d9bc2c06aec1d72f490188bbb40b
Thibault Mercier <thib.mercier@ymail.com>
2023-03-16 17:14:44 +0100

    great infowindow and toggle

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/mapbox/streets-v10",
     })
 
     this.#addMarkersToMap()
@@ -22,7 +22,11 @@ export default class extends Controller {
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
+      const popup = new mapboxgl.Popup({
+        closeButton: false,
+        className: 'trip-activity-popup',
+        maxWidth: '400px'
+      }).setHTML(marker.info_window_html)
       new mapboxgl.Marker()
       .setLngLat([marker.lng, marker.lat])
       .setPopup(popup)
