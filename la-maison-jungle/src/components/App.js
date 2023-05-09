4662e8a0f38a3e3f2c39ed1b61ef251a1d40beaf
Thibault Mercier <thib.mercier@ymail.com>
2023-05-09 18:34:28 +0200

    la maison jungle

---
+++ la-maison-jungle/src/components/App.js
@@ -0,0 +1,11 @@
+import logo from '../logo.svg';
+import '../App.css';
+import Banner from './Banner'
+import Cart from './Cart';
+import ShoppingList from './ShoppingList';
+
+function App() {
+  return <div><Banner /><Cart /><ShoppingList /></div>
+}
+
+export default App;
---
+++ la-maison-jungle/src/datas/plantList.js
@@ -0,0 +1,51 @@
+export const plantList = [
+	{
+		name: 'monstera',
+		category: 'classique',
+		id: '1ed'
+	},
+	{
+		name: 'ficus lyrata',
+		category: 'classique',
+		id: '2ab'
+	},
+	{
+		name: 'pothos argenté',
+		category: 'classique',
+		id: '3sd',
+		isSpecialOffer: true
+	},
+	{
+		name: 'yucca',
+		category: 'classique',
+		id: '4kk'
+	},
+	{
+		name: 'olivier',
+		category: 'extérieur',
+		id: '5pl',
+		isAvailable: false
+	},
+	{
+		name: 'géranium',
+		category: 'extérieur',
+		id: '6uo'
+	},
+	{
+		name: 'basilique',
+		category: 'extérieur',
+		id: '7ie',
+		isSpecialOffer: true
+	},
+	{
+		name: 'aloe',
+		category: 'plante grasse',
+		id: '8fp',
+		isSpecialOffer: true
+	},
+	{
+		name: 'succulente',
+		category: 'plante grasse',
+		id: '9vn'
+	}
+]
